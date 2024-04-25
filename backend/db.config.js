ec1bffc4dc4a24d51a42915fb591d07e45daa390
Ritesh S Hiremath <101038279+Ritesh2351235@users.noreply.github.com>
2024-04-25 08:43:51 +0530

    Update db.config.js

--- backend/db.config.js
+++ backend/db.config.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose")
 const connectDB = async () => {
-  await mongoose.connect("Your mongoose cluster link")
+  await mongoose.connect(
+    process.env.CONNECTION_STRING
+  );
   console.log(`${mongoose.connection.host} is connected`)
 }
 
